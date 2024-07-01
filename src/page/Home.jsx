7d419f5686756e7bddb2f3273bfb29728a5ef14a
BijianWu <384318185@qq.com>
2024-07-02 00:53:25 +0100

    adding home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -7,12 +7,46 @@ export default function Home(){
     return <>
         <h1>Home page</h1>
 
+        {/* <Grid container spacing={2}>
+            <Grid item xs={8}>
+                <Item>xs=8</Item>
+            </Grid>
+            <Grid item xs={4}>
+                <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={4}>
+                <Item>xs=4</Item>
+            </Grid>
+            <Grid item xs={8}>
+                <Item>xs=8</Item>
+            </Grid>
+        </Grid> */}
+
         <Card sx={{ maxWidth: 345 }}>
             <CardActionArea onClick={ () => navigate("/my-todos")}>
                 <CardMedia
                 component="img"
-                height="140"
+                height="200"
                 image={require("../assets/images/todo-lists.jpg")}
+                alt="green iguana"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    My Todos
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    click here to view all your todos
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea onClick={ () => navigate("/my-todos")}>
+                <CardMedia
+                component="img"
+                height="200"
+                image={require("../assets/images/heaven.jpg")}
                 alt="green iguana"
                 />
                 <CardContent>
