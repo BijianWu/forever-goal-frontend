764a9f8972116fe5110949432a69a22ff5147337
BijianWu <384318185@qq.com>
2024-07-02 00:44:58 +0100

    add in home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,29 @@
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 export default function Home(){
+    const navigate = useNavigate();
+
     return <>
         <h1>Home page</h1>
+
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea onClick={ () => navigate("/my-todos")}>
+                <CardMedia
+                component="img"
+                height="140"
+                image={require("../assets/images/todo-lists.jpg")}
+                alt="green iguana"
+                />
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    My Todos
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    click here to view all your todos
+                </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     </>
 }
